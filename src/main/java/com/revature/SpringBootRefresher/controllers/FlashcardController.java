0110cf414246becdc28b514866d482235e6d6be4
Daiki Anjiki <daikianjiki@gmail.com>
2023-06-27 09:36:51 -0700

    added basic CRUD

--- src/main/java/com/revature/SpringBootRefresher/controllers/FlashcardController.java
+++ src/main/java/com/revature/SpringBootRefresher/controllers/FlashcardController.java
@@ -1,15 +1,52 @@
 package com.revature.SpringBootRefresher.controllers;
 
+import com.revature.SpringBootRefresher.models.Flashcard;
+import com.revature.SpringBootRefresher.repos.FlashcardRepo;
 import com.revature.SpringBootRefresher.services.FlashcardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FlashcardController {
     FlashcardService flashcardService;
 
     @Autowired
-    FlashcardController(FlashcardService flashcardService) {
+    public FlashcardController(FlashcardService flashcardService) {
         this.flashcardService = flashcardService;
     }
+
+    @GetMapping("flashcard/{id}")
+    public Optional<Flashcard> getFlashcard(@PathVariable int id) {
+        return flashcardService.getFlashcard(id);
+    }
+
+    @GetMapping("flashcards")
+    public List<Flashcard> getAllFlashcards() {
+        return flashcardService.getAllFlashcards();
+    }
+
+    @PostMapping("flashcard")
+    public Flashcard createFlashcard(@RequestBody Flashcard flashcard) {
+        return flashcardService.createFlashcard(flashcard);
+    }
+
+    @DeleteMapping("flashcard/{id}")
+    public void deleteFlashcard(@PathVariable int id) {
+        flashcardService.deleteFlashcard(id);
+    }
+
+    @PutMapping("flashcard/{id}")
+    public Flashcard updateFlashcard(@PathVariable int id) {
+        return flashcardService.updateFlashcard(id);
+    }
+
+    @PatchMapping("flashcard/{id}")
+    public Flashcard patchFlashcard(@PathVariable int id) {
+        return flashcardService.patchFlashcard(id);
+    }
+
 }
--- src/main/java/com/revature/SpringBootRefresher/services/FlashcardService.java
+++ src/main/java/com/revature/SpringBootRefresher/services/FlashcardService.java
@@ -1,8 +1,12 @@
 package com.revature.SpringBootRefresher.services;
 
 
+import com.revature.SpringBootRefresher.models.Flashcard;
 import com.revature.SpringBootRefresher.repos.FlashcardRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FlashcardService {
@@ -10,5 +14,35 @@ public class FlashcardService {
 
     public FlashcardService(FlashcardRepo flashcardRepo) {
         this.flashcardRepo = flashcardRepo;
+    }
+
+
+    public Optional<Flashcard> getFlashcard(int id) {
+        return flashcardRepo.findById(id);
+    }
+
+    public List<Flashcard> getAllFlashcards() {
+        return flashcardRepo.findAll();
+    }
+
+    public Flashcard createFlashcard(Flashcard flashcard) {
+        return flashcardRepo.save(flashcard);
+    }
+
+    public void deleteFlashcard(int id) {
+        flashcardRepo.deleteById(id);
+    }
+
+    public Flashcard updateFlashcard(int id) {
+        Flashcard flashcard = flashcardRepo.findById(id).get();
+        flashcard.setQuestion(flashcard.getQuestion());
+        flashcard.setAnswer(flashcard.getAnswer());
+        flashcard.setCategory(flashcard.getCategory());
+        return flashcardRepo.save(flashcard);
+    }
+
+    public Flashcard patchFlashcard(int id) {
+        Flashcard flashcard = flashcardRepo.findById(id).get();
+        return flashcardRepo.save(flashcard);
     }
 }
